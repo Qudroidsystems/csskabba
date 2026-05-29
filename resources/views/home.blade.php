0a7e700d98fc55a8e777b4be6c23ad3380a9e912
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-05-29 13:40:36 +0100

    working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
 
                         <div class="swiper-slide">
                             <div class="image-layer-three"
-                                style="background-image: url({{ asset('assets/assets/images/backgrounds/home.jpg')}});"></div>
+                                style="background-image: url({{ asset('assets/assets/images/backgrounds/home.jpeg')}});"></div>
                             <!-- /.image-layer -->
 
                             <div class="main-slider-three__shape-1"></div>
